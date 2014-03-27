565a41c1de8e90304d62dc21e960e60e6f58ddf8
DrJCFitz <john.c.fitzpatrick.dr@gmail.com>
2014-03-27 07:34:53 -0400

    removed PortalProfile dependency from JacksonTest

--- src/test/java/JacksonTest/JacksonTest.java
+++ src/test/java/JacksonTest/JacksonTest.java
@@ -35,18 +35,9 @@ public class JacksonTest {
 
 	@SuppressWarnings({ "serial", "deprecation" })
 	public static void main(String[] args) throws IOException {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext(
-				"classpath:portalProfile-config.xml");
-
-		PortalProfile ebatesProfile = appContext.getBean("ebatesProfile",
-				PortalProfile.class);
-
-		//List<Element> portalElements = getElements(ebatesProfile);
+				
 		String tagHtml = "<tr class='store'> \n <td class='fav'> \n  <div class='fav-n ui2012' id='Coastal.com' data-merch='9629'></div> </td> \n <td class='storeName'> <strong><a href='/coupons/coastal-com/index.htm'>Coastal.com Coupons &amp; Cash Back </a></strong> </td> \n <td class='storeRebate'> <a href='/coastal-com_9629-xfas?campaign_entity_id=15901061&amp;navigation_id=10003' rel='nofollow' target='_blank'>5.0% </a> </td> \n <td class='storeInfo shop'> <a href='/coastal-com_9629-xfas?campaign_entity_id=15901061&amp;navigation_id=10003' rel='nofollow' target='_blank' class='ui2012'></a> </td> \n</tr>";
 		String baseUri = "http://www.ebates.com";
-		//String elTag = portalElements.get(0).tagName();
-		//Don't set attribute - will set with double quotes.
-		//Attributes elAttrs = portalElements.get(0).attributes().clone();
 		
 		Element el = new Element(Tag.valueOf("tr"), baseUri);
 		el.prepend(tagHtml);
@@ -55,8 +46,6 @@ public class JacksonTest {
 		elementList.add(el);
 		elementList.add(el);
 		
-		//Merchant merch = new Merchant("name", "mkey", "link", new Reward(5.0,
-		//		"points"), new GregorianCalendar().getTimeInMillis(), true);
 		ObjectMapper mapper = new ObjectMapper();
 		SimpleModule module = new SimpleModule("myModule", new Version(1, 0, 0,
 				null));
@@ -65,24 +54,19 @@ public class JacksonTest {
 		mapper.registerModule(module);
 		JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, Element.class);
 		mapper.reader(type);
-		//mapper.reader(new TypeReference<List<Element>>(){});
 		mapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
-		//System.out.println(el.toString());
-		//System.out.println(el.select("td.storeName a").attr("href"));
-
+		
 		try {
 
 			// convert user object to json string, and save to a file
 			mapper.writeValue(
 					new File(
 							"c:\\Users\\John\\JavaEE\\sample-workflow\\portalElements.json"),
-					elementList);
+					el);
 
-			// display to console
-			// System.out.println(mapper.writeValueAsString(merch));
-			// System.out.println(mapper.writeValueAsString(portalElements));
-			List<Element> elList2 = mapper.readValue(new File("c:\\Users\\John\\JavaEE\\sample-workflow\\portalElements.json"), type);
-			System.out.println(elList2);
+			//List<Element> elList2 = mapper.readValue(new File("c:\\Users\\John\\JavaEE\\sample-workflow\\portalElements.json"), type);
+			Element el2 = mapper.readValue(new File("c:\\Users\\John\\JavaEE\\sample-workflow\\portalElements.json"), Element.class);
+			System.out.println(el2);
 
 		} catch (JsonGenerationException e) {
 
@@ -100,14 +84,5 @@ public class JacksonTest {
 
 	}
 
-	private static List<Element> getElements(PortalProfile profile)
-			throws IOException {
-		Document doc = Jsoup.connect(profile.getAllStoresUrl()).get();
-		Elements storeLinks = doc.select(profile.getRootElement());
-		List<Element> portalElement = storeLinks.subList(0,
-				storeLinks.size() - 1);
-		//System.out.println(portalElement.get(0));
-		return portalElement;
-	}
 
 }
